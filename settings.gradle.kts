066eaae1c141c8b5b6cdc705f510c95dc4ecaf28
abdalla ahmed <80337502+abdullahahmed6318@users.noreply.github.com>
2025-11-19 21:30:21 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+//dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositories {
+//        google()
+//        mavenCentral()
+//        maven("https://jitpack.io")
+//    }
+//}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io") // يجب أن يكون هكذا
+    }
+}
+
+rootProject.name = "app_goalrush"
+include(":app")
+ 
